b44693477be9ee28ebb08687bc96fee8ba21387c
GCalex <catalin_alex_g@yahoo.com>
2015-08-10 18:02:54 +0300

    watamidoing

--- apiPrintData.js
+++ apiPrintData.js
@@ -18,27 +18,5 @@ apiPrintData = (function() {
 })();
 
 module.exports = apiPrintData;
-}).call(this);
-
-
-
-// module.exports = function(){
-// 		function apiPrintData(btcModel){
-// 			this.btcModel = btcModel;
-// 			}
-// 		apiPrintData.prototype.getDataJson = function(data){
-// 			return this.btcModel.findAll().then(function(info){
-// 				info = info.map(function(d){
-// 					return d.get(null,{plain:true})
-// 				})
-// 				return data(null,JSON.stringify(info))
-// 			})
-// 		}
-	
-// 	return apiPrintData;
-// }
-// console.log(apiPrintData)
-// module.exports = apiPrintData;
-
-
+})();
 
